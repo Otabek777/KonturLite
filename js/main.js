289b9caeb0d39f5e53f6e17800b3d2039b92b5ae
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-06-29 20:50:59 +0500

    finish function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-'use strict';
+const btnNum = document.querySelector('.number'),
+      blockNum = document.querySelector('.number__block'),
+      searchOpen = document.querySelector('#searchOpen'),
+      searchBlock = document.querySelector('#searchBlock'),
+      searchClose = document.querySelector('#searchClose'),
+      body = document.querySelector('body');
+
+body.addEventListener('click', function() {
+    blockNum.classList.remove('active');
+});
+
+btnNum.addEventListener('click', function() {
+    setTimeout(function() {
+        blockNum.classList.add('active');
+    }, 0001)
+});
+
+searchOpen.addEventListener('click', function() {
+    searchBlock.classList.add('active');
+});
+searchClose.addEventListener('click', function() {
+    searchBlock.classList.remove('active');
+});
 
